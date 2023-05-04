52880cc3f1c83398f286bdd9c9d9b985a2dfc208
arnaizaitor <arnaizaitor@gmail.com>
2023-05-04 21:48:49 +0200

    first commit

---
+++ frontend/src/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <header className="Home-header">
+                    <h1>D.E.U.S. Home</h1>
+                </header>
+            </div>
+        );
+    }
+}
